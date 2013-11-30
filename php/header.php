35e9f28ee68c8ea2131395267038cfe725d30162
Chris Marasco <cxm6467@gmail.com>
2013-11-29 22:23:01 -0500

    ‘Merica

--- php/header.php
+++ php/header.php
@@ -5,6 +5,7 @@
 
             <html>
             <head>
+                <meta charset="UTF-8">
                 <title>Traffic Alert</title>
                 <link type="text/css" rel="stylesheet" href="lib/bootstrap/css/bootstrap-responsive.min.css" />
                 <script src="lib/bootstrap/js/bootstrap.min.js"></script>
