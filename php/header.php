fc4bbf372832806b747b12675e9822380fa8efd3
Chris Marasco <cxm6467@gmail.com>
2013-11-29 23:14:16 -0500

    engrish

--- php/header.php
+++ php/header.php
@@ -3,7 +3,7 @@
     echo('
         <!DOCTYPE html>
 
-            <html>
+            <html lang="en">
             <head>
                 <meta charset="UTF-8">
                 <title>Traffic Alert</title>
