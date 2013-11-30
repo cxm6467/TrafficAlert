b7a2388e976e4e9c9eccfcd9757250ac9c12591c
Chris Marasco <cxm6467@gmail.com>
2013-11-29 23:29:53 -0500

    no message

--- php/header.php
+++ php/header.php
@@ -10,7 +10,7 @@
                 <link type="text/css" rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css" />
                 <script src="lib/bootstrap/js/bootstrap.min.js"></script>
                 
-                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes">
             </head>
             <body>
             
