d721a4aa7198fcfc6bf817f5095cd00ecebe3655
Chris Marasco <cxm6467@gmail.com>
2013-11-29 23:24:46 -0500

    Stylin

--- php/header.php
+++ php/header.php
@@ -7,7 +7,7 @@
             <head>
                 <meta charset="UTF-8">
                 <title>Traffic Alert</title>
-                <link type="text/css" rel="stylesheet" href="lib/bootstrap/css/bootstrap-responsive.min.css" />
+                <link type="text/css" rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css" />
                 <script src="lib/bootstrap/js/bootstrap.min.js"></script>
                 
                 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
