53ebff208dda0bfc22a649b0136e9bb4e7c4028a
Chris Marasco <cxm6467@gmail.com>
2013-11-29 21:54:10 -0500

    pcpcpcpc

--- header.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    echo('link type="text/css" rel="stylesheet" href="lib/bootstrap/css/bootstrap-responsive.min.css" />');
-    echo('<script src="lib/bootstrap/js/bootstrap.min.js"></script>');
-?>
