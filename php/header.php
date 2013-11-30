c7857fe082e0eadc7d0d72e7dea6c695434a2d69
Chris Marasco <cxm6467@gmail.com>
2013-11-29 23:15:41 -0500

    mobile whaaaat

--- php/header.php
+++ php/header.php
@@ -9,6 +9,8 @@
                 <title>Traffic Alert</title>
                 <link type="text/css" rel="stylesheet" href="lib/bootstrap/css/bootstrap-responsive.min.css" />
                 <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+                
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
             </head>
             <body>
     ');
