eeef2784f154bb2d4f55e8df36aa4df59d15697c
Chris Marasco <cxm6467@gmail.com>
2013-12-05 13:00:57 -0500

    Content

--- php/header.php
+++ php/header.php
@@ -13,6 +13,36 @@
                 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes">
             </head>
             <body>
+                <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                    <div class="container">
+                      <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                          <span class="sr-only">Toggle navigation</span>
+                          <span class="icon-bar"></span>
+                          <span class="icon-bar"></span>
+                          <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">Traffic Alert</a>
+                      </div>
+                      <div class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav">
+                          <li class="active"><a href="#">Home</a></li>
+                          <li><a href="#about">About</a></li>
+                          <li><a href="#contact">Contact</a></li>
+                        </ul>
+                      </div><!--/.nav-collapse -->
+                    </div>
+                  </div>
+              
+                <div class="container">
+              
+                    <div class="starter-template">
+                      <h1>Traffic Alert</h1>
+                    </div>
+
+                </div>
+                <!-- /.container -->
+
             
     ');
 ?>
